8de79a4107a243fd5350a61fb756c2038e8465b6
xujinxing <xujinxing@zuoyebang.com>
2024-09-25 21:59:45 +0800

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  runtimeConfig: {
+    openaiApiKey: 'oNlw9NMA-c6a8ed2cdc1e623c57e90ff5fa4e1459936ba3dc'
+  },
+})
---
+++ server/api/chat.ts
@@ -0,0 +1,21 @@
+import { createOpenAI } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+export default defineLazyEventHandler(async () => {
+  const runTimeConfig = useRuntimeConfig();
+  const apiKey = runTimeConfig.openaiApiKey;
+
+  if (!apiKey) throw new Error('Missing OpenAI API key');
+  const openai = createOpenAI({
+    apiKey: apiKey
+  });
+
+  return defineEventHandler(async (event: any) => {
+    const { messages } = await readBody(event);
+    const result = await streamText({
+      model: openai('gpt-4-turbo'),
+      messages,
+    });
+
+    return result.toDataStreamResponse();
+  });
+});
