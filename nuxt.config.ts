115b957290883130cd936febdcadd6579df6e1c3
xujinxing <xujinxing@zuoyebang.com>
2024-09-26 11:16:55 +0800

    3.5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   runtimeConfig: {
-    openaiApiKey: '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'
+    openaiApiKey: 'sk-geWuhmYIwJgW1z75Dh5crWMKlc6nEqqs2OsHlmQ6LFGegxPn'
   },
 })
